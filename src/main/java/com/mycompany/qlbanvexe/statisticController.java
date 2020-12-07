6fdd0b5c85911bf90c11109388515ac4e3041c95
VuiTrinhThiKim <1851010157vui@ou.edu.vn>
2020-12-08 00:50:34 +0700

    update sql

--- src/main/java/com/mycompany/qlbanvexe/statisticController.java
+++ src/main/java/com/mycompany/qlbanvexe/statisticController.java
@@ -4,6 +4,40 @@
  * and open the template in the editor.
  */
 
+<<<<<<< HEAD
+
+package com.mycompany.qlbanvexe;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import java.sql.Connection;
+import com.mysql.jdbc.Driver;
+
+/**
+ *
+ * @author asus
+ */
+public class statisticController  extends loginController {
+    
+    
+    public void initialize(URL url, ResourceBundle rb) {
+       
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        
+    }  
+    @FXML
+    private void Add(ActionEvent eventA){
+       
+        
+    }
+    
+}
+=======
 //package com.mysql.jdbc;
 //import com.mycompany.qlbanvexe.loginController;
 //import java.net.URL;
@@ -36,3 +70,4 @@
 //    }
 //    
 //}
+>>>>>>> ef39591bfd70c9626301ecd8577efb082427e61a
